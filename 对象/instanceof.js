82eaec65017dd39241a295a445b2c7a8a8233765
EvilMoOd <962475158@qq.com>
2023-02-12 18:21:23 +0800

    手写小优化

--- 对象/instanceof.js
+++ 对象/instanceof.js
@@ -1,12 +1,10 @@
 // instanceOf
 // 检测constructor的prototype是否在obj的原型上
 const instanceOf = function (obj, constructor) {
-  let cp = obj.__proto__;
-  while (cp) {
-    if (constructor.prototype === cp) {
-      return true;
-    }
-    cp = cp.__proto__;
+  let proto = Object.getPrototypeOf(obj);
+  while (proto) {
+    if (proto === constructor.prototype) return true;
+    proto = Object.getPrototypeOf(proto);
   }
   return false;
 };
--- 对象/new.js
+++ 对象/new.js
@@ -1,8 +1,7 @@
 function myNew(constructor, ...args) {
   // 判断参数是否是一个函数
   if (typeof constructor !== 'function') {
-    console.error('type error');
-    return;
+    return console.error('type error');
   }
   // 新建一个空对象，对象的原型为构造函数的 prototype 对象
   const obj = Object.create(constructor.prototype);
